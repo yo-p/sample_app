310c83eb1f5cbbbda4dafb81798a10bac67036fd
yo-p <papapapipipi42@gmail.com>
2020-05-28 12:13:53 +0000

    Add test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-                      password: "foobar", password_confirmation: "foobar")
+                      password: "foobar", password_confirmation: "foobarvag")
   end
 
   test "should be valid" do
