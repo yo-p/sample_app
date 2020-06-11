f21f915da9390a8d45cc98b495d41c5a4048374b
yo-p <papapapipipi42@gmail.com>
2020-06-11 04:58:53 +0000

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,8 +36,9 @@ class User < ApplicationRecord
     end
 
     def activate
-        update_attribute(:activated, true)
-        update_attribute(:activated_at, Time.zone.now)
+        update_columns(activated: true, activated_at: Time.zone.now)
+        # update_attribute(:activated, true)
+        # update_attribute(:activated_at, Time.zone.now)
     end
 
     def send_activation_email
