e90aa1438add8639461a9466a46938489b49cc4f
yo-p <papapapipipi42@gmail.com>
2020-06-18 08:28:38 +0000

    Add test

--- app/models/user.rb
+++ app/models/user.rb
@@ -61,6 +61,11 @@ class User < ApplicationRecord
         reset_sent_at < 2.hours.ago
     end
 
+    #試作feedの定義
+    def feed
+        Micropost.where("user_id = ?", id)
+    end
+
     private
 
     def downcase_email
