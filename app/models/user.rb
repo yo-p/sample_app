0e56654c9ef7f04432cc77760a159e71682066b4
yo-p <papapapipipi42@gmail.com>
2020-06-26 06:07:14 +0000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,9 +69,11 @@ class User < ApplicationRecord
         reset_sent_at < 2.hours.ago
     end
 
-    #試作feedの定義
+    # ユーザーのステータスフィードを返す
     def feed
-        Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+        following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+        Micropost.where("user_id IN (#{following_ids}) 
+                        OR user_id = :user_id", user_id: id)
     end
 
     # ユーザーをフォローする
