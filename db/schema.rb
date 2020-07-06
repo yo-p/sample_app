9b44db9b68de7371ea2862acf9d3ce7c2bb6a861
yo-p <papapapipipi42@gmail.com>
2020-07-06 08:25:49 +0000

    Add Reply

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_064707) do
+ActiveRecord::Schema.define(version: 2020_07_06_075619) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_07_06_064707) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.integer "in_reply_to"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
