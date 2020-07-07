e3233bb40999ac8cb50236c0fbb850e0e2435b18
yo-p <papapapipipi42@gmail.com>
2020-07-07 11:44:55 +0000

    Add reply

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,10 +1,55 @@
 class Micropost < ApplicationRecord
+
+  before_validation :set_in_reply_to
+
   belongs_to :user
   default_scope -> {order(created_at: :desc)}
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
-  validate :picture_size
+  validates :in_reply_to, presence: false
+  validate :picture_size, :reply_to_user
+
+  def Micropost.including_replies(id)
+    where(in_reply_to: [id, 0]).or(Micropost.where(user_id: id))
+  end
+
+  def is_i?(s)
+    Integer(s) != nil rescue false
+  end
+
+  def set_in_reply_to
+    if @index = content.index("@")
+      reply_id = []
+      while is_i?(content[@index + 1])
+        @index += 1
+        reply_id << content[@index]
+      end
+      self.in_reply_to = reply_id.join.to_i
+    else 
+      self.in_reply_to = 0
+    end
+  end
+
+  def reply_to_user_name_correct?(user)
+    user_name = user.name.gsub(" ", "-")
+    content[@index + 2, user_name.length] == user_name
+  end
+
+  def reply_to_user
+    return if self.in_reply_to == 0
+    unless user = User.find_by(id: self.in_reply_to)
+      errors.add(:base, "User ID you specified doesn't exist.")
+    else
+      if user_id == self.in_reply_to
+        errors.add(:base, "You can't reply to yourself.")
+      else
+        unless reply_to_user_name_correct?(user)
+          errors.add(:base, "User ID doesn't match its name.")
+        end
+      end
+    end
+  end
 
   private
 
