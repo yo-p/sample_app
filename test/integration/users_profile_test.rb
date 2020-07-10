d6658d38d393d107e5c0d2a89648896a14450d13
yo-p <papapapipipi42@gmail.com>
2020-07-10 06:00:18 +0000

    modified

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -11,17 +11,50 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     get user_path(@user)
     assert_template 'users/show'
     assert_select 'title', full_title(@user.name)
+
+    # User info
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
+
+    # Follow info
+    assert_select 'a[href=?]', following_user_path(@user)
+    assert_match @user.following.count.to_s, response.body
+    assert_select 'a[href=?]', followers_user_path(@user)
+    assert_match @user.followers.count.to_s, response.body
+
+    # Microposts
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination', count: 1
-    @user.microposts.paginate(page: 1).each do |micropost|
+    @user.microposts.paginate(page:1).each do |micropost|
       assert_match micropost.content, response.body
     end
+
     # Micropost Search
     get user_path(@user), params: {q: {content_cont: "a"}}
     q = @user.microposts.ransack(content_cont: "a")
-    q.result.paginate(page: 1 ).each do |micropost|
+    q.result.paginate(page:1).each do |micropost|
+      assert_match micropost.content, response.body
+    end
+  end
+
+  test "home profile display" do
+    log_in_as(@user)
+    get root_path
+
+    # User info
+    assert_select 'h1', text: @user.name
+    assert_select '[class=?]', "gravatar"
+
+    # Follow info
+    assert_select 'a[href=?]', following_user_path(@user)
+    assert_match @user.following.count.to_s, response.body
+    assert_select 'a[href=?]', followers_user_path(@user)
+    assert_match @user.followers.count.to_s, response.body
+
+    # Micropost Search
+    get root_path, params: {q: {content_cont: "a"}}
+    q = @user.feed.ransack(content_cont: "a")
+    q.result.paginate(page:1).each do |micropost|
       assert_match micropost.content, response.body
     end
   end
