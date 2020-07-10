eafd5ef16143cd84c26e06cf0415483dc80f8182
yo-p <papapapipipi42@gmail.com>
2020-07-10 06:52:19 +0000

    modiefied UserModel

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ApplicationRecord
     
     has_many :from_messages, class_name: "Message",
                     foreign_key: "from_id", dependent: :destroy
-    has_many :to_messages, class_name: "Messages",
+    has_many :to_messages, class_name: "Message",
                     foreign_key: "to_id", dependent: :destroy
     has_many :sent_messages, through: :from_messages, source: :from
     has_many :received_messages, through: :to_messages, source: :to
